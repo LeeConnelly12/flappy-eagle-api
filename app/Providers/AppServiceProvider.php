c9809a3e2844ffc8ea88be202fd060649755389e
Lee Connelly <lee@leeconnelly.dev>
2023-03-19 12:32:53 +0000

    Unguard models

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,5 +21,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::preventLazyLoading(!$this->app->isProduction());
+        Model::unguard();
     }
 }
